a1cb48ce9f7a4180a13c9189805b9699d5f55dc5
AmrHAlali <amrohalali@gmail.com>
2025-07-12 15:51:38 +0300

    square color before reducers

---
+++ src/screens/SquareScreen.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import ColorCounter from '../components/ColorCounter';
+
+const COLOR_INCREMENT = 15;
+
+const SquareScreen = () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
+  const setColor = ({ color, change }: { color: string, change: number }): void => {
+    // color: 'red', 'green', 'blue'
+    // change: +15, -15
+
+    switch (color) {
+      case 'red':
+        red + change > 255 || red + change < 0
+          ? null
+          : setRed(red + change);
+        return;
+
+      case 'green':
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+        return;
+
+      case 'blue':
+        blue + change > 255 || blue + change < 0
+          ? null
+          : setBlue(blue + change);
+        return;
+        default:
+          return;
+    }
+  };
+
+  return (
+    <View>
+      <ColorCounter
+        onIncrease={() => setColor({ color: 'red', change: COLOR_INCREMENT })}
+        onDecrease={() => setColor({ color: 'red', change: -1 * COLOR_INCREMENT })}
+        color="Red"
+      />
+      <ColorCounter
+        onIncrease={() => setColor({ color: 'blue', change: COLOR_INCREMENT })}
+        onDecrease={() => setColor({ color: 'blue', change: -1 * COLOR_INCREMENT })}
+        color="Blue"
+      />
+      <ColorCounter
+        onIncrease={() => setColor({ color: 'green', change: COLOR_INCREMENT })}
+        onDecrease={() => setColor({ color: 'green', change: -1 * COLOR_INCREMENT })}
+        color="Green"
+      />
+
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red}, ${green}, ${blue})`
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SquareScreen;
